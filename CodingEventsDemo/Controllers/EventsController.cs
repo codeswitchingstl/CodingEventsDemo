ecbe48f536cf6d5f78ecb169d26324d42d211802
codeswitchingstl <codeswitching@icloud.com>
2022-10-18 19:53:21 -0500

    initial commit

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -22,5 +22,42 @@ namespace coding_events_practice.Controllers
 
             return View();
         }
+<<<<<<< Updated upstream
+=======
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc = "")
+        {
+            Events.Add(name, desc);
+
+            return Redirect("/Events");
+        }
+
+        [HttpGet]
+        [Route("/Events/Edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            Event editingEvent = EventData.GetById(eventId);
+            ViewBag.eventToEdit = editingEvent;
+            ViewBag.title = "Edit Event " + editingEvent.Name + "(id = " + editingEvent.Id + ")";
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            Event editingEvent = EventData.GetById(eventId);
+            editingEvent.Name = name;
+            editingEvent.Description = description;
+            return Redirect("/Events");
+        }
+>>>>>>> Stashed changes
     }
 }
