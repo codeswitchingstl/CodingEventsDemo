21bc8658f1d3726ad3c0fefb4e590e9ee2c42558
codeswitchingstl <42210389+codeswitchingstl@users.noreply.github.com>
2022-10-19 13:38:39 -0500

    Update EventsController.cs

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -1,7 +1,4 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,17 +7,26 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
+        private static readonly Dictionary<string, string> Events = new Dictionary<string, string>();
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<string> Events = new List<string>();
-            Events.Add("Code With Pride");
-            Events.Add("Apple WWDC");
-            Events.Add("Strange Loop");
-
             ViewBag.events = Events;
-
             return View();
+        }
+        
+        public IActionResult Add()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name, desc);
+            return Redirect("/Events");
         }
     }
 }
