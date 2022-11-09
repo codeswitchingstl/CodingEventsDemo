7eac71ea0fd97d1d30d5886d7cd5cf454e9f169e
codeswitchingstl <codeswitching@icloud.com>
2022-11-08 22:11:03 -0600

    text exercises update 2

--- CodingEventsDemo/Data/EventDbContext.cs
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -1,36 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using CodingEventsDemo.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodingEventsDemo.Data
 {
-    public class EventData
+    public class EventDbContext : DbContext
     {
-        static private Dictionary<int, Event> Events = new Dictionary<int, Event>();
+        public DbSet<Event> Events { get; set; }
 
-        // GetAll
-        public static IEnumerable<Event> GetAll()
+        public EventDbContext(DbContextOptions<EventDbContext> options)
+              : base(options)
         {
-            return Events.Values;
-        }
-
-        // Add
-        public static void Add(Event newEvent)
-        {
-            Events.Add(newEvent.Id, newEvent);
-        }
-
-        // Remove
-        public static void Remove(int id)
-        {
-            Events.Remove(id);
-        }
-
-        // GetById
-        public static Event GetById(int id)
-        {
-            return Events[id];
         }
     }
 }
